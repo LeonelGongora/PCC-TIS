7e9ba810b520b6b71959879c5b5cb1a18c95ca00
Elmer-David <dabur12vidbu345@gmail.com>
2023-12-07 22:31:43 -0400

    backend parcial notification

--- laravel_pcc/app/Http/Controllers/NotificationController.php
+++ laravel_pcc/app/Http/Controllers/NotificationController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Notification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
 
 class NotificationController extends Controller
 {
@@ -15,6 +17,64 @@ class NotificationController extends Controller
     public function index()
     {
         return Notification::all();
+    }
+
+    public function misNotificaciones($id){
+
+        $notificacionuser = DB::table('notifications')
+        ->join('notification_user', 'notifications.id', '=', 'notification_user.notification_id')
+        ->join('users', 'users.id', '=', 'notification_user.user_id')
+        // ->join('event_types', 'event_types.id', '=', 'events.event_type_id')
+        ->where('users.id', $id)
+        ->select('notifications.*')
+        ->get();
+
+        $notificacionteam = DB::table('notifications')
+        ->join('notification_team', 'notifications.id', '=', 'notification_team.notification_id')
+        ->join('teams', 'teams.id', '=', 'notification_team.team_id')
+        ->join('users', 'users.id', '=', 'teams.id_coach')
+        ->where('users.id', $id)
+        ->select('notifications.*')
+        ->get();
+
+        // $results = array();
+        // $results[] = $events;
+        // $results[] = $equipos;
+
+        // $results = array();
+        // $r = array();
+        // $results[] = $events;
+        // $results[] = $equipos;
+        // $mapeo = function($elemento) {
+        //     return array(
+        //         'id' => $elemento['id'],
+        //         'nombre_evento' => $elemento['nombre_evento']
+        //         );
+        // };
+        // $r = array_map($mapeo, $results);
+        $r = array();
+        // $r = array_merge($notificacionuser, $notificacionteam);
+        $array = Arr::collapse([$notificacionuser, $notificacionteam]);
+        // $orde = sort($array, int '$created_at' = SORT_REGULAR): true { }
+
+        // $ordenados = $array->sortByDesc('created_at')->paginate(5);
+        // $prueba = array.sort((a, b) => new Date(a.fechas).getTime() > new Date(b.fechas).getTime());
+        // $ordenados = $array::latest()-get();
+
+        // $e = array_merge()
+        // $mapeo = function($elemento) {
+        //     return array(
+        //         'id' => $elemento['id'],
+        //         'nombre_evento' => $elemento['nombre_evento']
+        //         );
+        // };
+        // $r = array_map($mapeo, array($events, $equipos));
+
+        return response()->json([
+            'status' => 200,
+            'notifications' => $array,
+
+        ]);
     }
 
     /**
--- react_pcc/src/components/DropdownNotification.js
+++ react_pcc/src/components/DropdownNotification.js
@@ -1,18 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../stylesheets/Dropdown.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import Cookies from "universal-cookie";
+import {URL_API} from '../const';
+
+const cookies = new Cookies();
+const Notificacion_Url = `${URL_API}/misnotificaciones`;
 
 function DropdownNotification({setOpenDropFath, isOpen}) {
   //const [isOpen, setIsOpen] = useState(false);
+  const [notification, setNotification] = useState([])
 
   const toggleDropdown = () => {
     if (isOpen) {
       setOpenDropFath(null);
     } else {
       setOpenDropFath("notification");
-    }
+    } 
   };
+
+  useEffect(() => {
+    getNotification();
+  },[])
+
+  const getNotification=async()=>{
+    if(cookies.get('id_usuario') === '' || cookies.get('id_usuario')=== undefined){
+      console.log(`${cookies.get('id_usuario')} No se encuentra registrado`)
+    }else{
+      const id = cookies.get('id_usuario');
+      const url = `${Notificacion_Url}/${id}`;
+      const response = await axios.get(url)
+      setNotification(response.data.notifications)
+      console.log(response.data.notifications)
+      console.log(id)
+    }
+  }
+
   return (
     <div className="dropdown-container">
       <button
@@ -25,11 +50,23 @@ function DropdownNotification({setOpenDropFath, isOpen}) {
         <FontAwesomeIcon className="dropdownIcon-notification" icon={faBell} />
       </button>
       {isOpen && (
-        <ul className="dropdown-menu-notification">
-          <li>Notificacion 1</li>
-          <li>Notificacion 2</li>
-          <li>Notificacion 3</li>
-        </ul>
+        
+            <ul className="dropdown-menu-notification">
+              {notification[0] != null ? (
+                <>
+                {notification.map((n) => {  
+                      return (<div key={n.id}>
+                        {n.informacion === null ? (
+                            <li>{n.contenido}</li>
+                        ) : (
+                            <li>{`${n.contenido}. Mayor Informacion: ${n.informacion}`}</li>
+                        )}
+                      </div>);
+                })}</>
+              ):(
+                <li>No tiene notificaciones</li>
+              )}  
+            </ul>
       )}
     </div>
   );
