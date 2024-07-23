623f6871992662215acdf75def489b0bdd08e93e
Leunam77 <juan.ma6531@gmail.com>
2024-07-23 12:32:25 -0400

    commit de correcion deployado

--- react_pcc/src/const.js
+++ react_pcc/src/const.js
@@ -1,4 +1,4 @@
-// export const URL_API = "http://blackcloud.tis.cs.umss.edu.bo/api"
-// export const URL_IMG = "http://blackcloud.tis.cs.umss.edu.bo"
 export const URL_API = "https://sansicup.umss.edu.bo/api"
 export const URL_IMG = "https://sansicup.umss.edu.bo"
+//export const URL_API = "https://sansicup.umss.edu.bo/api"
+//export const URL_IMG = "https://sansicup.umss.edu.bo"
